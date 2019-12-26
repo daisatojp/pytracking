ad5f2c22a767448abda9e0f201df0fb50c0d6e5b
Dai Sato <sato-da@sanritz.co.jp>
2019-12-26 20:23:14 +0900

    fix

--- pytracking/run_images.py
+++ pytracking/run_images.py
@@ -1,44 +1,15 @@
 import os
 from os.path import join, splitext, basename
-
-import sys
 import argparse
 import cv2
 from glob import glob
+from pytracking.evaluation import Tracker
 from pycvlib.utility.pascalvoc import read_pascalvoc
 
-env_path = os.path.join(os.path.dirname(__file__), '..')
-if env_path not in sys.path:
-    sys.path.append(env_path)
 
-from pytracking.evaluation import Tracker
-from ltr.admin.loading import torch_load_legacy
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str)
-    parser.add_argument('--srcdir', type=str)
-    parser.add_argument('--dstdir', type=str)
-    args = parser.parse_args()
-
-    weights_path = args.weights
-    src_dir = args.srcdir
-    dst_dir = args.dstdir
-
-    out_dir = join(dst_dir, basename(src_dir))
+def test(tracker_, src_dir, out_dir):
     if not os.path.exists(out_dir):
         os.mkdir(out_dir)
-
-    tracker = Tracker('dimp', 'dimp50')
-
-    params = tracker.get_parameters()
-    params.tracker_name = 'dimp'
-    params.param_name = 'dimp50'
-    params.net.net_path = weights_path
-
-    tracker_ = tracker.tracker_class(params)
-    tracker_.initialize_features()
 
     img_paths = glob(join(src_dir, '*.jpg'))
     img_path0 = img_paths[0]
@@ -53,6 +24,11 @@ def main():
 
     img = cv2.imread(img_path0)
     tracker_.initialize(img, {'init_bbox': init_bbox})
+    frame_disp = img.copy()
+    cv2.rectangle(
+        frame_disp, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 255, 0), 5)
+    cv2.imwrite(join(out_dir, 'frame{0:04d}.jpg'.format(0)), frame_disp)
+
     for i in range(1, len(img_paths)):
         img_path = img_paths[i]
         frame = cv2.imread(img_path)
@@ -63,6 +39,38 @@ def main():
             frame_disp, (state[0], state[1]), (state[2] + state[0], state[3] + state[1]),
             (0, 255, 0), 5)
         cv2.imwrite(join(out_dir, 'frame{0:04d}.jpg'.format(i)), frame_disp)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', type=str)
+    parser.add_argument('--srcdir', type=str)
+    parser.add_argument('--dstdir', type=str)
+    parser.add_argument('--dirs', action='store_true')
+    args = parser.parse_args()
+
+    weights_path = args.weights
+    src_dir = args.srcdir
+    dst_dir = args.dstdir
+    dirs = args.dirs
+
+    tracker = Tracker('dimp', 'dimp50')
+    params = tracker.get_parameters()
+    params.tracker_name = 'dimp'
+    params.param_name = 'dimp50'
+    params.net.net_path = weights_path
+    tracker_ = tracker.tracker_class(params)
+    tracker_.initialize_features()
+
+    if not dirs:
+        out_dir = join(dst_dir, basename(src_dir))
+        test(tracker_, src_dir, out_dir)
+    else:
+        src_dirs = glob(join(src_dir, '*'))
+        for src_dir in src_dirs:
+            if os.path.isdir(src_dir):
+                out_dir = join(dst_dir, basename(src_dir))
+                test(tracker_, src_dir, out_dir)
 
 
 if __name__ == '__main__':
